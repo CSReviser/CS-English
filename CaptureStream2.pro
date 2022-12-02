9f90ecfb2975fd2d300591071230cca7349c126d
CSReviser <CSReviser@gmai.com>
2022-12-02 21:40:50 +0900

    mp3向けffmpegの-write_xing 0オプション追加

---
+++ CaptureStream2.pro
@@ -0,0 +1,66 @@
+# -------------------------------------------------
+# Project created by QtCreator 2009-04-20T14:05:07
+# -------------------------------------------------
+QT += network \
+	xml \
+	xmlpatterns \
+	script
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = CaptureStream2
+TEMPLATE = app
+SOURCES += main.cpp \
+	mainwindow.cpp \
+	downloadthread.cpp \
+	downloadmanager.cpp \
+	customizedialog.cpp \
+	messagewindow.cpp \
+	scrambledialog.cpp \
+	utility.cpp \
+	mp3.cpp \
+	urldownloader.cpp
+HEADERS += mainwindow.h \
+	downloadthread.h \
+	downloadmanager.h \
+	customizedialog.h \
+	messagewindow.h \
+	scrambledialog.h \
+	utility.h \
+	mp3.h \
+	urldownloader.h \
+	qt4qt5.h
+FORMS += mainwindow.ui \
+	customizedialog.ui \
+	messagewindow.ui \
+	scrambledialog.ui
+
+linux {
+	QMAKE_LFLAGS += -no-pie
+}
+
+macx {
+	QMAKE_CFLAGS_RELEASE += -fvisibility=hidden
+	QMAKE_CXXFLAGS_RELEASE += -fvisibility=hidden
+
+	x86 {
+		CONFIG += ppc
+		QMAKE_CC = gcc-4.0
+		QMAKE_CXX = g++-4.0
+		QMAKE_MAC_SDK = /Developer/SDKs/MacOSX10.12u.sdk
+		QMAKE_CFLAGS_RELEASE += -mmacosx-version-min=10.12
+		QMAKE_CXXFLAGS_RELEASE += -mmacosx-version-min=10.12
+	}
+
+	release {
+		QMAKE_POST_LINK += macdeployqt CS-English.app
+	}
+}
+
+OTHER_FILES += \
+	stylesheet-mac.qss \
+	stylesheet-win.qss \
+	stylesheet-ubu.qss
+
+RESOURCES += \
+    stylesheet.qrc
--- downloadthread.cpp
+++ downloadthread.cpp
@@ -120,13 +120,13 @@ DownloadThread::DownloadThread( Ui::MainWindowClass* ui ) : isCanceled(false), f
 	if ( ffmpegHash.empty() ) {
 		ffmpegHash["aac"] = "%1,-vn,-acodec,copy,%2";
 		ffmpegHash["m4a"] = "%1,-id3v2_version,3,-metadata,title=%3,-metadata,artist=NHK,-metadata,album=%4,-metadata,date=%5,-metadata,genre=Speech,-vn,-bsf,aac_adtstoasc,-acodec,copy,%2";
-		ffmpegHash["mp3-64k-S"] = "%1,-id3v2_version,3,-metadata,title=%3,-metadata,artist=NHK,-metadata,album=%4,-metadata,date=%5,-metadata,genre=Speech,-vn,-acodec:a,libmp3lame,-ab,64k,-ac,2,%2";
-		ffmpegHash["mp3-128k-S"] = "%1,-id3v2_version,3,-metadata,title=%3,-metadata,artist=NHK,-metadata,album=%4,-metadata,date=%5,-metadata,genre=Speech,-vn,-acodec:a,libmp3lame,-ab,128k,-ac,2,%2";
-		ffmpegHash["mp3-48k-S"] = "%1,-id3v2_version,3,-metadata,title=%3,-metadata,artist=NHK,-metadata,album=%4,-metadata,date=%5,-metadata,genre=Speech,-vn,-acodec:a,libmp3lame,-ab,48k,-ac,2,%2";
-		ffmpegHash["mp3-40k-M"] = "%1,-id3v2_version,3,-metadata,title=%3,-metadata,artist=NHK,-metadata,album=%4,-metadata,date=%5,-metadata,genre=Speech,-vn,-acodec:a,libmp3lame,-ab,40k,-ar,32k,-ac,1,%2";
-		ffmpegHash["mp3-32k-M"] = "%1,-id3v2_version,3,-metadata,title=%3,-metadata,artist=NHK,-metadata,album=%4,-metadata,date=%5,-metadata,genre=Speech,-vn,-acodec:a,libmp3lame,-ab,32k,-ar,32k,-ac,1,%2";
-		ffmpegHash["mp3-24k-M"] = "%1,-id3v2_version,3,-metadata,title=%3,-metadata,artist=NHK,-metadata,album=%4,-metadata,date=%5,-metadata,genre=Speech,-vn,-acodec:a,libmp3lame,-ab,24k,-ar,22.05k,-ac,1,%2";
-		ffmpegHash["mp3-16k-M"] = "%1,-id3v2_version,3,-metadata,title=%3,-metadata,artist=NHK,-metadata,album=%4,-metadata,date=%5,-metadata,genre=Speech,-vn,-acodec:a,libmp3lame,-ab,16k,-ar,22.05k,-ac,1,%2";
+		ffmpegHash["mp3-64k-S"] = "%1,-id3v2_version,3,-metadata,title=%3,-metadata,artist=NHK,-metadata,album=%4,-metadata,date=%5,-metadata,genre=Speech,-vn,-acodec:a,libmp3lame,-ab,64k,-ac,2,-write_xing,0,%2";
+		ffmpegHash["mp3-128k-S"] = "%1,-id3v2_version,3,-metadata,title=%3,-metadata,artist=NHK,-metadata,album=%4,-metadata,date=%5,-metadata,genre=Speech,-vn,-acodec:a,libmp3lame,-ab,128k,-ac,2,-write_xing,0,%2";
+		ffmpegHash["mp3-48k-S"] = "%1,-id3v2_version,3,-metadata,title=%3,-metadata,artist=NHK,-metadata,album=%4,-metadata,date=%5,-metadata,genre=Speech,-vn,-acodec:a,libmp3lame,-ab,48k,-ac,2,-write_xing,0,%2";
+		ffmpegHash["mp3-40k-M"] = "%1,-id3v2_version,3,-metadata,title=%3,-metadata,artist=NHK,-metadata,album=%4,-metadata,date=%5,-metadata,genre=Speech,-vn,-acodec:a,libmp3lame,-ab,40k,-ar,32k,-ac,1,-write_xing,0,%2";
+		ffmpegHash["mp3-32k-M"] = "%1,-id3v2_version,3,-metadata,title=%3,-metadata,artist=NHK,-metadata,album=%4,-metadata,date=%5,-metadata,genre=Speech,-vn,-acodec:a,libmp3lame,-ab,32k,-ar,32k,-ac,1,-write_xing,0,%2";
+		ffmpegHash["mp3-24k-M"] = "%1,-id3v2_version,3,-metadata,title=%3,-metadata,artist=NHK,-metadata,album=%4,-metadata,date=%5,-metadata,genre=Speech,-vn,-acodec:a,libmp3lame,-ab,24k,-ar,22.05k,-ac,1,-write_xing,0,%2";
+		ffmpegHash["mp3-16k-M"] = "%1,-id3v2_version,3,-metadata,title=%3,-metadata,artist=NHK,-metadata,album=%4,-metadata,date=%5,-metadata,genre=Speech,-vn,-acodec:a,libmp3lame,-ab,16k,-ar,22.05k,-ac,1,-write_xing,0,%2";
 	}
 
 	if ( processError.empty() ) {
