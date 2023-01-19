aa6a00fca44cdb4d9bee7d822bc520cb57281350
CSReviser <CSReviser@gmai.com>
2023-01-19 23:30:19 +0900

    ベータテスト版β1.4

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -131,7 +131,7 @@ namespace {
 //			int day = regexp.cap( 2 ).toInt();
 //			result = QString( " (%1/%2/%3)" ).arg( regexp.cap( 3 ) )
 //					.arg( month, 2, 10, QLatin1Char( '0' ) ).arg( day, 2, 10, QLatin1Char( '0' ) );
-			result = QString( "  (2023/01/18) -β１.4-" ); 
+			result = QString( "  (2023/01/19) -β１.4-" ); 
 		}
 		return result;
 	}
